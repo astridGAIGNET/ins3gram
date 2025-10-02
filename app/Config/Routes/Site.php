1a58199e4282724728ed3c6398eaf6601b291e4b
astridgaignet <a.gaignet@live.fr>
2025-10-02 15:32:25 +0200

    feat: user front

--- app/Config/Routes/Site.php
+++ app/Config/Routes/Site.php
@@ -4,12 +4,18 @@ $routes->get('/forbidden', 'Site::forbidden');
 $routes->get('/test-pagination','Site::testPagination');
 
 $routes->get('/sign-in', 'Auth::signIn');
+$routes->get('/register', 'User::register');
 $routes->post('/auth/login', 'Auth::login');
 $routes->get('/auth/logout', 'Auth::logout');
 
 $routes->group('recette', function ($routes) {
     $routes->get('/', 'Recipe::index');
     $routes->get('(:any)', 'Recipe::show/$1');
+});
+
+$routes->group('user', function ($routes) {
+    $routes->post('insert', 'User::insert');
+    $routes->post('update', 'User::update');
 });
 
 $routes->post('/datatable/searchdatatable', 'DataTable::searchdatatable');
